2852afbea97ce45578cc7f127eb451ebc18da840
hanhqvu <hanhqvu@users.noreply.github.com>
2024-05-26 21:27:07 +0900

    refactor: instance group

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-08e32db9e33e28876"
-  instance_type          = "t2.nano"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-08e32db9e33e28876"
+  instance_type   = "t2.nano"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                 #!/bin/bash
@@ -13,11 +13,6 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "terraform-example"
-  }
 }
 
 resource "aws_security_group" "instance" {
